90f6108b85e0ab8412aecbe3adc0ec925be8d949
Zhang YiYang <120240967+LeonardoSya@users.noreply.github.com>
2024-06-18 17:00:15 +0800

    feat: useTransition 实现过渡动画

--- 搜索框-取消上一次请求/src/App.tsx
+++ 搜索框-取消上一次请求/src/App.tsx
@@ -10,7 +10,7 @@
 
 // fetch中封装了signal的事件监听，当在任意地方调用controller.abort()时，对应的请求就会取消
 
-import { Suspense, useState, use } from "react"
+import { Suspense, useState, use, useTransition } from "react"
 import './App.css'
 
 const postApi = (query: number) => {
@@ -35,31 +35,35 @@ export default function App() {
   const [api, setApi] = useState(postApi(0))  // 将postApi执行返回的promise作为返回结果存在state中
   const [query, setQuery] = useState(0)
 
+  const [isPending, startTransition] = useTransition()
+
   const inputChange = (e) => {  //* input输入时，取消上一次的请求，并发送新的请求
     const newQuery = e.target.value
     setQuery(newQuery)
 
-    api.cancel()
-    setApi(postApi(newQuery))
+    startTransition(() => {
+      api.cancel()
+      setApi(postApi(newQuery))
+    })
   }
 
   return (
     <>
       <input type="text" placeholder="search by number" onChange={inputChange} />
       <Suspense fallback={<div>loading...</div>}>
-        <List api={api} />
+        <List api={api} isPending={isPending}/>
       </Suspense>
     </>
   )
 }
 
-const List = ({ api }) => {
+const List = ({ api,isPending }) => {
   const posts = use(api)
 
   return (
     <ul>
       {posts.map((post) => (
-        <div key={post.id}>
+        <div key={post.id} style={{opacity:isPending?0.5:1}}>
           <h2>{post.title}</h2>
           <p>postId: {post.postId}</p>
           <p>id: {post.id}</p>
--- 点击按钮更新数据/src/App.tsx
+++ 点击按钮更新数据/src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useState, use } from "react"
+import { Suspense, useState, use, useTransition } from "react"
 
 const getApi = async () => {
   const res = await fetch('https://api.chucknorris.io/jokes/random')
@@ -13,14 +13,15 @@ const getApi = async () => {
 
 export default function App() {
   // const [api, setApi] = useState<Promise<unknown> | null>(null)
-  const _initApi = getApi()  // 初始化时创建一个Promise
-  const [api, setApi] = useState(_initApi)  // 将Promise作为状态存储
+  const [api, setApi] = useState(getApi)  // 初始化一个Promise并将Promise作为状态存储
   // 更简洁的状态设计，也是 React 19 所倡导的开发思路。
 
+  const [isPending, startTransition] = useTransition()
 
   const __clickToGetMessage = () => {
-    // 每次点击，都会创建新的promise并更新状态
-    setApi(getApi())
+    startTransition(() => {
+      setApi(getApi())  // 每次点击，都会创建新的promise并更新状态
+    })
   }
 
   return (
@@ -28,20 +29,21 @@ export default function App() {
       <button onClick={__clickToGetMessage}>获取数据</button>
       <div>
         <Suspense fallback={<div>loading...</div>}>
-          <Item api={api} />
+          <Item api={api} isPending={isPending} />
         </Suspense>
       </div>
     </>
   )
 }
 
-const Item = (props: { api: Promise<unknown> | null }) => {
+const Item = (props: {api: Promise<unknown>, isPending: boolean}) => {
+  const {isPending, api} = props
 
   //* api的初始值是null，在Item子组件中直接把api传给use肯定会报错，
   //* 因为Suspense只会捕获子组件的 promise 异常，而不是所有异常
   //* 因此，在Item组件中，我们需要做一个判断，当api为null时直接返回，这样让use(null)得不到执行的时机
 
-  if (!props.api) {
+  if (!api) {
     console.log('初始化 api===null')
     return (
       <div>nothing</div>
@@ -51,6 +53,6 @@ const Item = (props: { api: Promise<unknown> | null }) => {
   // use() 允许在组件渲染过程中调用异步函数，如果异步操作尚未完成，React会自动暂停组件的渲染
   const joke = use(props.api)
   return (
-    <div>{joke.value}</div>
+    <div style={{opacity: isPending? 0.5:1}}>{joke.value}</div>
   )
 }
